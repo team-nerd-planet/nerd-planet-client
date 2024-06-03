f9b9066a4dcb3e7bb544d7c245832d9fd87f5383
kiwon <chigomuh@gmail.com>
2024-06-03 20:06:37 +0900

    fix: revalidate api route auth token을 검증합니다

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,7 +1,14 @@
-import { NextRequest } from "next/server";
 import { revalidateTag } from "next/cache";
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => {
+  const authorization = headers().get("Authorization");
+
+  if (process.env.REVALIDATE_SECRET !== authorization) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   const tags = request.nextUrl.searchParams.getAll("tag");
 
   tags.forEach(revalidateTag);
