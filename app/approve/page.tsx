3802254a54d6c087bc9d65819703dc21b740129e
chigomuh <tngusrldnjs1@gmail.com>
2024-06-05 23:28:08 +0900

    feat: 이메일 승인 기능을 구현합니다
    - 이메일 구독 기능을 server action으로 리팩토링합니다

---
+++ app/approve/page.tsx
@@ -0,0 +1,62 @@
+import { approveSubscription } from "services/subscription/queries";
+
+const ApprovePage = async ({
+  searchParams: { token },
+}: {
+  searchParams: {
+    token?: string;
+  };
+}) => {
+  if (!token) {
+    return <InvalidRequest />;
+  }
+
+  const approveResult = await approveSubscription(token);
+
+  const isApprove = "ok" in approveResult && approveResult.ok;
+
+  if (isApprove) {
+    return <ApproveSuccess />;
+  }
+
+  return <ApproveFail />;
+};
+
+export default ApprovePage;
+
+const InvalidRequest = () => {
+  return (
+    <div className="flex center w-full h-screen text-xl font-bold text-white py-[36px]">
+      잘못된 요청입니다 😢
+      <br />
+      <br />
+      이메일 인증을 위한 링크가 올바르지 않아요.
+      <br />
+      다시 한 번 확인해주세요.
+    </div>
+  );
+};
+
+const ApproveSuccess = () => {
+  return (
+    <div className="flex center w-full h-screen text-xl font-bold text-white py-[36px]">
+      이메일이 인증되었어요! 🎉
+      <br />
+      <br />
+      이제부터 나만의 피드를 받아 볼 수 있어요.
+    </div>
+  );
+};
+
+const ApproveFail = () => {
+  return (
+    <div className="flex center w-full h-screen text-xl font-bold text-white py-[36px]">
+      이메일 인증에 실패했어요 😢
+      <br />
+      <br />
+      이미 인증된 이메일이거나, 인증 토큰이 만료되었을 수 있어요.
+      <br />
+      다시 한 번 시도해주세요.
+    </div>
+  );
+};
---
+++ components/bottomSheet/actions.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import { applySubscription } from "services/subscription/queries";
+
+export const subscriptionAction = async ({
+  division,
+  email,
+  name,
+  preferredCompanySizeArr,
+  preferredCompanyArr,
+  preferredJobArr,
+  preferredSkillArr,
+}: {
+  division: string;
+  email: string;
+  name: string;
+  preferredCompanySizeArr: number[];
+  preferredCompanyArr: number[];
+  preferredJobArr: number[];
+  preferredSkillArr: number[];
+}) => {
+  // 먼저 이메일이 유효한지 확인
+  if (!email) {
+    return {
+      ok: false,
+      message: "이메일을 입력해주세요",
+    };
+  }
+
+  // 구독신청
+  const result = await applySubscription({
+    division,
+    email,
+    name,
+    preferredCompanySizeArr,
+    preferredCompanyArr,
+    preferredJobArr,
+    preferredSkillArr,
+  });
+
+  if (result.ok) {
+    return {
+      ok: true,
+      message: "구독이 완료되었습니다.",
+    };
+  }
+
+  return {
+    ok: false,
+    message: result.message || "Failed to subscribe",
+  };
+};
