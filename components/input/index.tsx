6dab23f85fd5f20889f8d2533eca085a2d4a3dbd
thewoowon <thewoowon@naver.com>
2024-06-08 17:10:51 +0900

    feat: company name 입력 수정 및 타입 수정

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,12 +1,14 @@
 import { cn } from "lib/utils";
+import React from "react";
 import { type InputHTMLAttributes } from "react";
 
-const Input = ({
-  className,
-  ...props
-}: InputHTMLAttributes<HTMLInputElement>) => {
+const Input = React.forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>(({ className, ...props }, ref) => {
   return (
     <input
+      ref={ref}
       className={cn(
         "border border-border rounded-[5px] bg-background text-xs indent-2 placeholder:font-normal placeholder-[#8E8E93] focus:outline-none focus:ring-0 focus:border-primary w-full h-[40px] px-2 py-0.5",
         className
@@ -15,6 +17,8 @@ const Input = ({
       {...props}
     />
   );
-};
+});
+
+Input.displayName = "Input";
 
 export default Input;
